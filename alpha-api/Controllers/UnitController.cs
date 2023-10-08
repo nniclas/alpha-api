7d1b34efa28d4a1a4d80eb253f22caf8f3fdcb31
nniclas <niclas.oes@gmail.com>
2023-10-09 00:02:39 +0200

    added response types docs to endpoints. WIP

--- alpha-api/Controllers/UnitController.cs
+++ alpha-api/Controllers/UnitController.cs
@@ -4,6 +4,7 @@ using alpha_api.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace alpha_api.Controllers
 {
@@ -20,13 +21,35 @@ namespace alpha_api.Controllers
             this.service = service;
         }
 
+        /// <summary>
+        /// Retrieve all units
+        /// </summary>
+        /// <returns>Returns all units</returns>
+        /// <returns>Returns 200 OK success</returns>
+        /// <returns>Returns 400 Bad Request error</returns>
+        /// <returns>Returns 500 Internal Server Error</returns>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Unit>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<IEnumerable<Unit>>> Get()
         {
             return await this.service.GetAllAsync();
         }
 
+
+        /// <summary>
+        /// Retrieve unit by id
+        /// </summary>
+        /// <param name="id">The id of the unit to be retrieved</param>
+        /// <returns>Returns a unit</returns>
+        /// <returns>Returns 200 OK success</returns>
+        /// <returns>Returns 400 Bad Request error</returns>
+        /// <returns>Returns 500 Internal Server Error</returns>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Unit))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<Unit>> Get(int id)
         {
             var unit = await service.GetAsync(id);
@@ -34,13 +57,36 @@ namespace alpha_api.Controllers
             return unit;
         }
 
+        /// <summary>
+        /// Add a unit
+        /// </summary>
+        /// <param name="unit">The unit to add</param>
+        /// <returns>Returns a unit</returns>
+        /// <returns>Returns 201 Created success</returns>
+        /// <returns>Returns 400 Bad Request error</returns>
+        /// <returns>Returns 500 Internal Server Error</returns>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<Unit>> Post(Unit unit)
         {
             var u = await service.AddAsync(unit);
             return u;
         }
 
+        /// <summary>
+        /// Updates a unit
+        /// </summary>
+        /// <param name="id">The id of the unit to update</param>
+        /// <param name="unit">The unit to be updated</param>
+        /// <returns>Returns a unit</returns>
+        /// <returns>Returns 200 OK success</returns>
+        /// <returns>Returns 400 Bad Request error</returns>
+        /// <returns>Returns 500 Internal Server Error</returns>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Unit))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpPut("{id}")]
         public async Task<ActionResult<Unit>> Put(int id, Unit unit)
         {
@@ -51,6 +97,18 @@ namespace alpha_api.Controllers
             return unit;
         }
 
+
+        /// <summary>
+        /// Deletes a unit
+        /// </summary>
+        /// <param name="id">The id of the unit to remove</param>
+        /// <returns>Returns a boolean</returns>
+        /// <returns>Returns 200 OK success</returns>
+        /// <returns>Returns 400 Bad Request error</returns>
+        /// <returns>Returns 500 Internal Server Error</returns>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpDelete("{id}")]
         public async Task<bool> Delete(int id)
         {
